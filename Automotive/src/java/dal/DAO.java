4a208f67d3eed30e887f8d791ceb7c58f0c64f1e
nguyenchihuong <chihuong8a1@gmail.com>
2022-07-11 23:49:24 +0700

    update5

--- Automotive/src/java/dal/DAO.java
+++ Automotive/src/java/dal/DAO.java
@@ -336,4 +336,109 @@ public class DAO {
         return null;
     }
 //Viet Thai -------end------------
+
+    //Duc -----start-------
+    public List<Account> getAllAccount() {
+        List<Account> list = new ArrayList<>();
+        String query = "select * from accounts";
+        try {
+            conn = new DBContext().connection;
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Account(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getInt(5)));
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+
+    public int getTotalAccount() {
+        String query = "select count(*) from accounts";
+        try {
+            conn = new DBContext().connection;
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                return rs.getInt(1);
+            }
+        } catch (Exception e) {
+        }
+        return 0;
+    }
+
+    public List<Account> pagingAccount(int index) {
+        List<Account> list = new ArrayList<>();
+        String query = "select * from accounts \n"
+                + "order by id \n"
+                + "offset ? rows fetch next 6 rows only";
+        try {
+            conn = new DBContext().connection;
+            ps = conn.prepareStatement(query);
+            ps.setInt(1, (index - 1) * 6);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Account(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getInt(4),
+                        rs.getInt(5)));
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+
+    public Account getAccountByPID(String id) {
+        String query = "select * from accounts \n"
+                + "where uID = ?";
+        try {
+            conn = new DBContext().connection;
+            ps = conn.prepareStatement(query);
+            ps.setString(1, id);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                return new Account(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getInt(4),
+                        rs.getInt(5));
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    public void editAccount(String user, String pass, int isSell, int isAdmin) {
+        // Them vao de edit account
+        String query = "update accounts\n"
+                + "set [user] = ?,\n"
+                + "[pass] = ?,\n"
+                + "isSell = ?,\n"
+                + "isAdmin = ?,\n"
+                + "where uid = ?";
+        try {
+            conn = new DBContext().connection;//mo ket noi voi sql
+            ps = conn.prepareStatement(query);
+            ps.setString(1, user);
+            ps.setString(2, pass);
+            ps.setInt(3, isSell);
+            ps.setInt(4, isAdmin);
+            ps.executeUpdate();
+        } catch (Exception e) {
+        }
+    }
+
+    public void deleteAccount(String pid) {
+        String query = "delete from accounts \n"
+                + "where UID = ?";
+        try {
+            conn = new DBContext().connection;
+            ps = conn.prepareStatement(query);
+            ps.setString(1, pid);
+            ps.executeUpdate();
+        } catch (Exception e) {
+        }
+    }
+    ///  Duc ---End---
 }
